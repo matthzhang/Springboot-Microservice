2032e4f621bbe4f0d24c651becb8e977a95c5f3f
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-05-23 10:29:45 -0500

    integration testing, get test may be incorrect

--- product-service/src/main/java/com/programmingtechie/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/programmingtechie/productservice/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.programming.techie.productservice.controller;
 
+import com.programmingtechie.productservice.dto.ProductResponse;
 import com.programmingtechie.productservice.dto.ProductRequest;
 import com.programmingtechie.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,5 +20,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.ok)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
--- product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.programming.techie.productservice.controller;
 
 import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
 import com.programmingtechie.productservice.model.Product;
 import com.programmingtechie.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service;
 @RequiredArgsConstructor
@@ -23,5 +26,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        //products.stream().map(product -> mapToProductResponse(product)).toList();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+            .id(product.getId())
+            .name(product.getName())
+            .description(product.getDescription())
+            .price(product.getPrice())
+            .build();
     }
 }
