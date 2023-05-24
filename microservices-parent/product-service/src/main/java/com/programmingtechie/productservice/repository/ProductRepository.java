5b0a6078265b8c27da30ca314982488dce23fbb1
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-05-23 21:38:03 -0500

    start order service

--- microservices-parent/product-service/src/main/java/com/programmingtechie/productservice/repository/ProductRepository.java
+++ microservices-parent/product-service/src/main/java/com/programmingtechie/productservice/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.programming.techie.productservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-package com.programming.techie.productservice.model.MongoRepository;
+import com.programming.techie.productservice.model.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
