60769a6412e51f869348f5de3a9ec3f36d79ac39
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-05-24 12:38:11 -0500

    finished order service

--- microservices-parent/order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ microservices-parent/order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -1,16 +1,22 @@
 package com.programmingtechie.orderservice.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import com.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController{
 
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
         return "Order placed successfully";
     }
 }
