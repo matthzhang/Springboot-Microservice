7c73842b6cf4f712c4e7fe8ded68c6382dd47d09
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-07-07 14:16:49 -0500

    minor updates

--- microservices-new/product-service/src/main/java/com/programming/techie/productservice/repository/ProductRepository.java
+++ microservices-new/product-service/src/main/java/com/programming/techie/productservice/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.programming.techie.productservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import com.programming.techie.productservice.model.MongoRepository;
+import com.programming.techie.productservice.model.Product;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
